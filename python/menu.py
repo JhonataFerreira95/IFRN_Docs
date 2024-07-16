a576c62c7c8c23f2575fd1ab2557f440614dc901
JhonataOvirowa <jhonataovirowa@gmail.com>
2024-07-16 17:52:35 -0300

    Feito o menu.py

---
+++ python/menu.py
@@ -0,0 +1,10 @@
+print ("Ecolha uma das alternativas")
+
+menu = input("1-Água\n2-Cerveja\n3-Suco\n")
+
+if "1" in menu:
+    print("Você escolheu água.")
+elif "2" in menu:
+    print("Você escoheu cerveja.")
+elif "3" in menu:
+    print("Você escoheu suco.")
