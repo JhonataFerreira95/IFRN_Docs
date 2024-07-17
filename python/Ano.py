0e5ef0bd4f772360f91997811bf84dbdd24ce046
JhonataOvirowa <jhonataovirowa@gmail.com>
2024-07-16 21:58:20 -0300

    Ano bissexto

--- python/Ano.py
+++ python/Ano.py
@@ -1,8 +1,10 @@
 var = int(input("Digite o ano para saber se ele e bissexto: "))
 
 result = var % 400
+result2 = var % 4
+result3 = var % 100
 
-if result == 0:
+if result == 0 or result2 == 0 or result3 == 0:
     print("O ano é bissexto")
-elif result != 0:
+elif result != 0 or result2 != 0 or result3 != 0:
     print("O ano não é bissexto!")
