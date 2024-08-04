461bf0040f6fb74be7533071737d6da7547eb0c3
JhonataOvirowa <jhonataovirowa@gmail.com>
2024-08-04 20:30:05 -0300

    Feito indice de massa

--- ILP Condicionais/indice_massa.py
+++ ILP Condicionais/indice_massa.py
@@ -1,6 +1,15 @@
-peso = float(input("Digite o seu peso: "))
-altura = float(input("Digite a sua altura: "))
+peso = float(input("Digite o seu peso (Kg): "))
+altura = float(input("Digite a sua altura (M): "))
 
 calculo = peso / (altura * altura)
 
-print(calculo)
+if calculo <= 18.5:
+    print("Seu IMC é {:.2f}, abaixo do peso.".format(calculo))
+elif 18.5 < calculo <= 24.9:
+    print("Seu IMC é {:.2f}, peso ideal.".format(calculo))
+elif 25 <= calculo <= 29.9:
+    print("Seu IMC é {:.2f}, sobrepeso.".format(calculo))
+elif 30 <= calculo <= 39.9:
+    print("Seu IMC é {:.2f}, obesidade.".format(calculo))
+else:
+    print("Seu IMC é {:.2f}, obesidade mórbida.".format(calculo))
