032b5830b380eb6c845250c45d5b902f72d52575
JhonataOvirowa <jhonataovirowa@gmail.com>
2024-08-08 16:01:52 -0300

    Iniciado

---
+++ ILP Condicionais/game.py
@@ -0,0 +1,11 @@
+jogada = int(input("Suas ações:\n [1]Tessoura\n [2]Papel\n [3]Pedra\n")).lower()
+
+
+
+if 1 in jogada == tessoura:
+    print("JO")
+    print("KEN")
+    print("PO!!!")
+    print("=============|||=============")
+    print("Computador jogou PAPEL!")
+    print("=============|||=============")
