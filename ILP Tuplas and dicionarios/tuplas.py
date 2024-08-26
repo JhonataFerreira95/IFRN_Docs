16508fc88adcb0e6031b30f3b16c50f0e8c391aa
JhonataOvirowa <jhonataovirowa@gmail.com>
2024-08-26 15:53:27 -0300

    Q2

--- ILP Tuplas and dicionarios/tuplas.py
+++ ILP Tuplas and dicionarios/tuplas.py
@@ -1,3 +1,4 @@
+#tuplas:
 # 1. Escreva um programa que receba uma tupla de números inteiros e como
 # saída uma nova tupla com os elementos pares da tupla original. Por
 # exemplo, se a tupla for (1, 2, 3, 4, 5), o programa deve imprimir (2, 4).
@@ -10,3 +11,20 @@ for numero in tupla_original:
         tupla_pares += (numero,)
 
 print(tupla_pares)
+
+# 2. Escreva um programa que receba uma tupla de strings e exiba uma nova
+# tupla com as strings ordenadas alfabeticamente e sem repetições. Por
+# exemplo, se a tupla for ("banana", "maçã", "laranja", "banana", "uva"), o
+# programa deve imprimir ("banana", "laranja", "maçã", "uva").
+
+tupla_original = ("banana", "maçã", "laranja", "banana", "uva")
+tupla_unica = set(tupla_original)
+tupla_ordenada = tuple(sorted(tupla_unica))
+
+print(tupla_ordenada)
+
+# Dicionários:
+# 3. Escreva um programa que receba um dicionário que mapeia nomes de
+# países para suas populações e exiba o nome do país com a maior
+# população. Por exemplo, se o dicionário for {"Brasil": 211.8, "China":
+# 1400.5, "Índia": 1366.4}, o programa deve exibir "China".
